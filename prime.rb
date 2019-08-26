23a0d08ded91633f37945e3f19b598fd5bb503c4
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-26 16:25:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+   if integer <=1 
+     false 
+   else
+      (2..(integer - 1)).each do |n|
+      return false if integer % n == 0
+    end
+        integer == 2 
+    true 
+  end
+end
+
+
+
